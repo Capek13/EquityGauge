f16f0048dd1247f4140beef33d0b2b62b6064757
Jaromír Cejpek <mira.cejpek@gmail.com>
2025-06-15 21:08:20 +0200

    add click on accept all cokies and get PE

--- backend/scraper.py
+++ backend/scraper.py
@@ -1,8 +1,13 @@
 # scraper.py
-
+from bs4 import BeautifulSoup
 import re
 import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import  expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+import time
 
 class YahooFinanceScraper:
     TARGETED_URL = "https://finance.yahoo.com/quote/"
@@ -13,7 +18,7 @@ class YahooFinanceScraper:
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
         'Connection': 'keep-alive',
         }
-    REQUESTS_TIMEOUT = 2  # seconds
+    REQUESTS_TIMEOUT = 5  # seconds
     
     def __init__(self, ticker: str):
         """
@@ -26,7 +31,7 @@ class YahooFinanceScraper:
         self.ticker_targeted_url = f"{self.TARGETED_URL}{self.ticker}/" 
         self.soup = None # Will hold the BeautifulSoup object after fetching the page
 
-    def _fetch_page(self, url: str)-> BeautifulSoup | None:
+    def _fetch_page_request(self, url: str)-> BeautifulSoup | None:
         """
         Fetches the content of a given URL and returns a BeautifulSoup object or None if an error occurs.
         """
@@ -53,6 +58,44 @@ class YahooFinanceScraper:
             print(f"An unexpected error occurred during page fetch for {self.ticker}: {e}")
             return None
     
+    def _fetch_page_selenium(self, url: str)-> BeautifulSoup | None:
+        """
+        Fetches the content of a given URL using Selenium and returns a BeautifulSoup object or None if an error occurs.
+        """
+         # Set up Selenium WebDriver with Chrome options
+        options = Options()
+        options.add_argument("--headless")  # Run in headless mode (no GUI)
+        options.add_argument("--disable-gpu")
+        options.add_argument("--no-sandbox")
+        
+         # Initialize the WebDriver 
+        driver = webdriver.Chrome(options=options)
+
+        try:
+            # Open the URL
+            driver.get(self.ticker_targeted_url)
+            try:
+                accept_button = WebDriverWait(driver, self.REQUESTS_TIMEOUT).until(
+                    # Wait for the "Accept All" button to be clickable
+                    # By.XPATH is used to locate the button by its text, // is for find elements anywhere on page
+                    # sometimes is in button tag, sometimes in a div or span -> //button//span[contains(text(), 'Accept All')]
+                    EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Accept all')]"))
+                )
+                accept_button.click()  # Click the "Accept All" button if it appears
+                print(f"Clicked 'Accept All' button for {self.TARGETED_URL}")
+            except Exception as e:
+                print(f"No 'Accept All' button found for {self.TARGETED_URL} or it was not clickable: {e}")
+
+            # Wait for the page to load completely
+            time.sleep(self.REQUESTS_TIMEOUT) 
+            # Get the page source
+            page_content = driver.page_source
+            print(f"Successfully fetched page for {self.ticker} from {url}")
+            return BeautifulSoup(page_content, 'html.parser')
+        except Exception as e:
+            print(f"An unexpected error occurred during page fetch for {self.ticker}: {e}")
+            return None
+        
     def get_pe_ratio(self) -> str | None:
         """
         Fetches the P/E ratio for the stock ticker.
@@ -61,13 +104,13 @@ class YahooFinanceScraper:
         if not self.soup:
             # Fetch the page content if not already done
             print(f"Fetching P/E ratio for {self.ticker} from {self.ticker_targeted_url}")
-            self.soup = self._fetch_page(self.ticker_targeted_url)
+            self.soup = self._fetch_page_selenium(self.ticker_targeted_url)
             if not self.soup:
                 print(f"Failed to fetch page for {self.ticker}.")
                 return None
         try:
-            pe_row_header = self.soup.find('p', string=re.compile(r'Trailing P/E\s*', re.IGNORECASE))
-
+            pe_row_header = self.soup.find('p', string=re.compile(r'Trailing P/E', re.IGNORECASE))
+            print(pe_row_header)
             if pe_row_header:
                 # Find the next sibling <p> element which contains the P/E value
                 pe_value_element = pe_row_header.find_next_sibling('p')
