625781e38c3e564605a3c865d68ffb0d774567bd
Jaromír Cejpek <mira.cejpek@gmail.com>
2026-06-05 19:43:35 +0200

    feat: integrate SQLAlchemy for database operations and update company endpoints

--- backend/main.py
+++ backend/main.py
@@ -4,8 +4,12 @@ from fastapi import FastAPI, Request, status
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from backend.scraper import YahooFinanceSeleniumDriver,YahooFinanceScraper
-from backend.data_manager import DataManager
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from fastapi import Depends
+from backend.database import get_db
+from backend.models import Ticker, PERatios, Base, engine
+from datetime import date
 
 class TickerRequest(BaseModel):
     ticker: str
@@ -16,7 +20,7 @@ class TickerRequest(BaseModel):
     watchlist: bool = True
     industry: str | None = None
 
-dm = DataManager("backend/tickers.yaml")
+Base.metadata.create_all(engine)
 
 # Create an instance of the FastAPI application
 @asynccontextmanager
@@ -52,47 +56,57 @@ async def read_root():
     return {"message": "Hello, World!"}
 
 @app.get("/companies")
-async def get_companies():
-    data = dm.load_yaml("tickers")
-    return {"companies": data.get("tickers", [])}
+async def get_companies(db: Session = Depends(get_db)):
+    return db.query(Ticker).all()
 
 @app.get("/companies/{ticker}/pe")
-async def get_pe_ratio(ticker, request: Request):
-    scraper = YahooFinanceScraper(ticker,request.app.state.driver.driver)
-    pe = scraper.get_pe_ratio()
-    if pe is None:
-        return JSONResponse(
-            status_code=status.HTTP_404_NOT_FOUND,
-            content={"pe": pe}
-        )
-    return {"pe": pe}
+async def get_pe_ratio(ticker: str, request: Request, db: Session = Depends(get_db)):
+    last_pe = db.query(PERatios).filter(PERatios.ticker == ticker).order_by(PERatios.creation_date.desc()).first()
+    if last_pe is None or last_pe.creation_date < date.today():
+        scraper = YahooFinanceScraper(ticker,request.app.state.driver.driver)
+        pe = scraper.get_pe_ratio()
+        if pe is None:
+            return JSONResponse(
+                status_code=status.HTTP_404_NOT_FOUND,
+                content={"pe": pe}
+            )
+        db.add(PERatios(ticker=ticker, pe=pe, creation_date=date.today()))
+        db.commit()
+    else:
+        pe = last_pe.pe
 
+    return {"pe": pe}
+    
 @app.post("/companies",status_code=status.HTTP_201_CREATED)
-async def create_ticker(ticker: TickerRequest, request: Request):
-    if dm.get_ticker(ticker.ticker, "tickers") is not None:
+async def create_ticker(ticker: TickerRequest, request: Request, db: Session = Depends(get_db)):
+    if db.query(Ticker).filter(Ticker.ticker == ticker.ticker).first():
         return JSONResponse(
             status_code=status.HTTP_409_CONFLICT,
             content={"message": "Company with this ticker already exists."}
         )
-    dm.add_ticker_yaml(ticker.model_dump(), "tickers")
-    if dm.get_ticker(ticker.ticker, "tickers") is None:
-        return {"message": "FAIL - data wasn't added"}
-    return {"message": f"Ticker '{ticker.ticker}' created.", "url": f"{request.base_url}companies/{ticker.ticker}"}
+    db.add(Ticker(**ticker.model_dump()))
+    db.commit()
+    if db.query(Ticker).filter(Ticker.ticker == ticker.ticker).first():
+        return {"message": f"Ticker '{ticker.ticker}' created.", "url": f"{request.base_url}companies/{ticker.ticker}"}
+    return {"message": "FAIL - data wasn't added"}
 
 @app.delete("/companies/{ticker}")
-async def delete_ticker(ticker: str, request: Request):
-    if dm.get_ticker(ticker, "tickers") is None:
+async def delete_ticker(ticker: str, request: Request, db: Session = Depends(get_db)):
+    obj = db.query(Ticker).filter(Ticker.ticker == ticker).first()
+    if not obj:
         return JSONResponse(
             status_code=status.HTTP_404_NOT_FOUND,
             content={"message": f"Ticker '{ticker}' not found."}
         )
-    dm.remove_ticker_yaml(ticker, "tickers")
-    if dm.get_ticker(ticker, "tickers") is not None:
-        return JSONResponse(
+    db.delete(obj)
+    db.commit()
+    if not db.query(Ticker).filter(Ticker.ticker == ticker).first():
+        return {"message": f"Ticker '{ticker}' was removed successfully."}
+    return JSONResponse(
             status_code=status.HTTP_404_NOT_FOUND,
             content={"message": "FAIL - data wasn't removed"}
         )
-    return {"message": f"Ticker '{ticker}' was removed successfully."}
+
 
 
 # fastapi dev backend/main.py
